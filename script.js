b7632fd2f82e675035e83b08774ad12c15c97a69
yoanmarcos <yomarcos@free.fr>
2020-10-26 10:17:24 +0100

    use simpsons's API and generate ramdom quotes when refresh page

---
+++ script.js
@@ -0,0 +1,20 @@
+function fetchSimpsonJSON() {
+    const url = `https://simpsons-quotes-api.herokuapp.com/quotes`;
+    axios.get(url)
+        .then(function (response) {
+            return response.data[0]; // response.data instead of response.json() with fetch
+        })
+        .then(function (simpson) {
+            console.log('data decoded from JSON:', simpson);
+
+            // Build a block of HTML
+            const simpsonHtml = `
+          <p><strong>${simpson.character}</strong></p>
+          <p>${simpson.quote}</p>
+          <img src="${simpson.image}" />
+        `;
+            document.querySelector('#simpsons-quotes').innerHTML = simpsonHtml;
+        });
+}
+
+fetchSimpsonJSON();
